62ec8e26d425f980a532d9fba888f2abc9c22206
Redster11 <42357111+Redster11@users.noreply.github.com>
2020-03-25 13:06:30 -0700

    added Sieve Implementation

---
+++ HW5/Sieve of Eratosthenes algorithm/singleThread.java
@@ -0,0 +1,34 @@
+public class singleThread
+{
+    public void run(int num)
+    {
+        int count = 0;
+        int[] numbers = new int[num-2];
+        for(int i =0; i < numbers.length; i++)
+            numbers[i] = i+2;
+        for(int i = 0; i < numbers.length; i++)
+        {
+            int prime =numbers[i]; // grab the first value in the array will forsure be prime
+            if (prime == -1)
+                continue;
+            if(count == 9)
+            {
+                System.out.println();
+                count = 0;
+            }
+            System.out.print(prime + "\t");
+            count++;
+            for(int j =i + 1; j < numbers.length; j++)
+            {
+                if(numbers[j] % prime == 0)
+                    numbers[j] = -1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        singleThread sT = new singleThread();
+        long sTime = System.currentTimeMillis();
+        sT.run(1000000); 
+        System.out.println("The total Time elapsed: " + (System.currentTimeMillis() - sTime));
+    }
+}
